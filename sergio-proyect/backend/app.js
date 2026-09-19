3a248fefe26d4cee6497ec9ea29b3aa90fc9b062
DoVaH68 <juanjosefuentes.montecervino@gmail.com>
2026-09-19 10:25:18 -0500

    arreglo final
    asdadasd

--- sergio-proyect/backend/app.js
+++ sergio-proyect/backend/app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 const cors = require('cors');
 require('dotenv').config();
 
-var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var routeClientes = require('./routes/clientes');
 var routeProductos = require('./routes/productos');
@@ -19,13 +18,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/users', usersRouter);
 app.use('/clientes', routeClientes);
 app.use('/productos', routeProductos);
 app.use('/ventas', routeVentas);
-app.use('/', indexRouter);
+
+// --- Sirve el frontend ya compilado ---
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 app.use(function(req, res, next) {
   if (req.method === 'GET' && !req.path.startsWith('/users') && !req.path.startsWith('/clientes') && !req.path.startsWith('/productos') && !req.path.startsWith('/ventas')) {
