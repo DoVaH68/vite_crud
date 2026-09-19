7c1c0ee51cc8f965375f8b7c1c2aa9c4f3f32430
DoVaH68 <juanjosefuentes.montecervino@gmail.com>
2026-09-19 10:18:15 -0500

    ya sirviendo
    adasdad

--- sergio-proyect/backend/app.js
+++ sergio-proyect/backend/app.js
@@ -27,9 +27,6 @@ app.use('/productos', routeProductos);
 app.use('/ventas', routeVentas);
 app.use('/', indexRouter);
 
-// --- Sirve el frontend ya compilado ---
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
-
 app.use(function(req, res, next) {
   if (req.method === 'GET' && !req.path.startsWith('/users') && !req.path.startsWith('/clientes') && !req.path.startsWith('/productos') && !req.path.startsWith('/ventas')) {
     return res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
